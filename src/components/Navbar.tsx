9d5d85bca036c03072743fc1ba19505af19d97d0
Matthew Taylor <matthewaptaylor@gmail.com>
2023-08-05 15:13:26 +1200

    fix: navbar code

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,17 +2,17 @@ export default function Navbar() {
   return (
     <div className="navbar bg-base-100 p-4">
       <div className="navbar-start flex flex-row gap-4">
-        <button class="btn btn-square btn-ghost">
+        <button className="btn btn-square btn-ghost">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
             viewBox="0 0 24 24"
-            class="inline-block w-5 h-5 stroke-current"
+            className="inline-block w-5 h-5 stroke-current"
           >
             <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
               d="M4 6h16M4 12h16M4 18h16"
             ></path>
           </svg>
