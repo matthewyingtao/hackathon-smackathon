b96b8cdd6f11e11c5dde2bb4ecc2331285639db9
Matthew Taylor <matthewaptaylor@gmail.com>
2023-08-05 12:28:22 +1200

    fix: fix daisyUI not loading

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ const App = () => {
   return (
     <>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      <button className="btn">Button</button>
+      <button className="btn btn-primary">Button</button>
     </>
   );
 };
