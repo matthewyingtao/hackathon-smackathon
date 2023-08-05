917dcea57be8ef6b9d93e6ce628daa3cc6658b5b
Matthew Taylor <matthewaptaylor@gmail.com>
2023-08-05 16:06:15 +1200

    feat: add more fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ import daisyui from "daisyui";
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    fontFamily: {
+      body: ['"Inknut Antiqua"', "system-ui", "sans-serif"],
+      display: ['"Imperial Script"', "system-ui", "sans-serif"],
+      accent: ['"Island Moments"', "system-ui", "sans-serif"],
+    },
   },
   plugins: [daisyui],
 };
