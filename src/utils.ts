cb79e3ac7584c46173174d54aef75deafdcae1ba
Jack Freeth <33736460+RHOMBI@users.noreply.github.com>
2023-08-06 13:49:50 +1200

    feat: degrees of separation and bfs

--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,4 @@
-import { User } from "./stores/user";
+import { User, getUser } from "./stores/user";
 
 export const toDataUrl = async (url: string): Promise<string> => {
   const response = await fetch(url);
@@ -18,3 +18,39 @@ export const displayFellowships = (user: User) =>
   parseInt(user.immediateFamily.length.toPrecision(1))
     ? " fellowships"
     : "+ fellowships");
+
+
+interface searchNode {
+  id: string;
+  distance: number;
+}
+
+export const RunSeparationBFS = (
+  userID: string,
+  searchForID: string,
+): string => {
+  const searchedUsers = new Set<string>();
+  const queue: searchNode[] = [{ id: userID, distance: 0 }];
+
+  while (queue.length > 0) {
+    const { id, distance } = queue.shift()!;
+
+    if (id === searchForID) {
+      return distance.toString();
+    }
+
+    if (searchedUsers.has(id)) {
+      continue;
+    } else {
+      searchedUsers.add(id);
+    }
+
+    const user = getUser(id)!;
+
+    for (const family of user.immediateFamily) {
+      queue.push({ id: family.userId, distance: distance + 1 });
+    }
+  }
+
+  return -1;
+};
